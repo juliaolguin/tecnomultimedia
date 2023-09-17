34ab2fb0e226257d348f95626ebb417ac851d0d9
juliaolguin <130079526+juliaolguin@users.noreply.github.com>
2023-09-16 21:35:55 -0300

    Delete tp4 directory

--- tp4/rana.js
+++
@@ -1,9 +0,0 @@
-function dibujarRana()  {
-  if (direccion == ARRIBA) {
-  image(ranaArriba, posX, posY);
-} else if (direccion == DERECHA) {
-  image(ranaDer, posX, posY);
-} else if (direccion == IZQUIERDA) {
-  image(ranaIzq, posX, posY);
-}
-}
